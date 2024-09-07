2263fb9a0314d705bf172fd856d8e84fd2752c35
ZNAXNOR <omkarsdalvi1@gmail.com>
2024-09-07 09:58:34 +0530

    Up for View.
    Created a proper about me on home page. Social Links are active.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html', './**/*.{html,svg}'],
   theme: {
     extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'pure-white': '#FFFFFF',
+        'pure-black': '#000000',
+        'github-black': '#24292F',
+        'linkedin-blue': '#0077B5',
+        'default-color': '#3944BC',
+      },
       fontFamily: {
+        'google': ["ProductSans"],
         'sans': ['Product', 'Mona', 'sans-serif']
-      }
+      },
+      variants: {
+        fill: ['hover', 'focus'],
+      },
     },
   },
   plugins: [],
