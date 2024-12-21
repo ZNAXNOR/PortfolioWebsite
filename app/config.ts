dcb3e8dc3907a361128ae2da0bf75a0e1bb0ff2e
omkarsdalvi1 <omkarsdalvi1@gmail.com>
2024-12-21 04:12:31 +0000

    Created Index & Global Pages
    Website should be ready for deployment.

---
+++ app/config.ts
@@ -0,0 +1,27 @@
+export const metaData = {
+    baseUrl: "https://omkar.odtechlab.com/",
+    title: "Omkar Portfolio",
+    name: "Omkar Dalvi",
+    profileImage: "/profile.jpg",
+    ogImage: "/profile.jpg",
+    description:"Hello! This is Omkar and welcome to my personal portfolio website!",
+    about:"Hello! This is Omkar and welcome to my personal portfolio website!",
+    preferedCareerLocation:"India, Remote",
+    openToRelocation:"Yes",
+    areasOfJobIntrest:"Cloud, Data Centre Operations, Solutions Architect, Cloud Sales",
+    skills: " "
+};
+  
+export const contactInfo = {
+    address: "Navi Mumbai, Maharashtra, India",
+    addressMap: "https://mapsapp.goo.gl/ZWY7Pe9v3SqeHUG89",
+    phone: "+91 913-639-9987"
+};
+  
+export const socialLinks = {
+    twitter: "https://x.com/Omkar_Dalvi2004",
+    github: "https://github.com/ZNAXNOR",
+    instagram: "https://www.instagram.com/znaxnor/",
+    linkedin: "https://www.linkedin.com/in/omkar-dalvi2004/",
+    resume: "https://drive.google.com/file/d/1F5QjBJcLfejqjMlDDNWAMQsxHe4LkMV1/view?usp=drive_link"
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,61 @@
 import Image from "next/image";
+import { socialLinks } from "./config";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <section className="container mx-auto relative flex flex-col lg:flex-row">
+      <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen">
+        
+        {/* Left Column */}
+        <div className="flex flex-col px-8 lg:px-16">
+          {/* Profile Photo */}
+          <a href={socialLinks.linkedin} target="_blank">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+              src="/profile.jpg" alt="Profile photo"
+              className="rounded-full bg-gray-100 block mx-auto"
+              unoptimized width={160} height={160} priority/>
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+          <a href="/resume">
+            <button className="mt-10 w-full rounded-md bg-transparent px-3.5 py-2.5 
+                    ring-2 ring-orange-500 hover:ring-0 focus:ring-gray-700 focus:ring-offset-4
+                    text-center text-sm font-semibold text-orange-500 hover:text-white
+                    bg-gradient-to-r hover:from-red-600 hover:to-orange-600">
+                      Resume
+            </button>
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        {/* Right Column - Form */}
+        <div className="flex flex-col">
+          {/* Text Content */}
+          <div className="lg:mt-5 text-center mt-5 mx-auto lg:text-left">
+            <h1 className="mb-8 text-5xl font-medium ms">Welcome to my 
+              <strong className="bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-orange-500"> Portfolio!</strong>
+            </h1>
+            <div className="prose prose-neutral dark:prose-invert text-balance">
+              <p>
+                Hello! This is Omkar and welcome to my portfolio website!
+              </p>
+              <p>
+                This website was designed with NextJS as framework for clean, fast, and lightweight tools
+                to showcase projects and skills effectively. I plan to add some new features in the future.
+                If you have any suggestions, feel free to leave them {" "} 
+                <a href="/suggestions" target="_blank">here.</a>
+              </p>
+              <p>
+                The site is open-source and customizable, making it a perfect starting point for 
+                unique portfolios. Explore my projects, skills, and more as you scroll. You can 
+                also access the website code and contribute {" "} 
+                <a href="https://github.com/ZNAXNOR/PortfolioWebsite/tree/NextJSapp" target="_blank">here.</a>
+              </p>
+              <p>
+                If you encounter any issue, feel free to let me know {" "} 
+                <a href="/feedback" target="_blank">here.</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
