62c752bd7f7c247e0cf27a9835bdf162b2cdfaff
omkarsdalvi1 <omkarsdalvi1@gmail.com>
2024-12-23 04:34:40 +0000

    Modified Resum

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,64 +1,77 @@
-'use client';
+"use client";
 
-import Link from "next/link";
 import { ThemeSwitch } from "./theme-switch";
-import { socialLinks  } from "../config";
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, 
-        MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { Router } from "next/router";
-import { usePathname } from 'next/navigation';
-import { useState } from "react";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { usePathname } from "next/navigation";
 
 const navigation = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Contact Me', href: '/contact', current: false },
-  { name: 'Blog', href: '/blog', current: false },
-  { name: 'Projects', href: '/projects', current: false },
-  { name: 'Photos', href: '/photos', current: false },
-]
+  { name: "Home", href: "/", current: true },
+  { name: "Contact Me", href: "/contact", current: false },
+  { name: "Blog", href: "/blog", current: false },
+  { name: "Projects", href: "/projects", current: false },
+  { name: "Photos", href: "/photos", current: false },
+];
 
 function classNames(...classes: (string | undefined | null | false)[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export function Navbar() {
   const pathname = usePathname();
   return (
     <Disclosure as="nav">
-      <div className="pb-20 px-10">
-        <div className="mx-auto space-x-4 my-auto max-w-7xl px-2">
+      <div className="pb-20 sm:px-10">
+        <div className="mx-auto space-x-4 my-auto max-w-7xl sm:px-2">
           <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+            <div className="absolute inset-y-0 left-0 flex items-center tablet:hidden">
               {/* Mobile menu button*/}
-              <DisclosureButton className="group relative inline-flex items-center justify-center 
+              <DisclosureButton
+                className="group relative inline-flex items-center justify-center 
                                 rounded-md p-2 text-gray-700 hover:bg-gray-100 dark:text-gray-400 
                                 dark:hover:bg-gray-700 hover:text-black dark:hover:text-white 
-                                focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              >
                 <span className="absolute -inset-0.5" />
                 <span className="sr-only">Open main menu</span>
-                <Bars3Icon aria-hidden="true" className="block size-6 group-data-[open]:hidden" />
-                <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-[open]:block" />
+                <Bars3Icon
+                  aria-hidden="true"
+                  className="block size-6 group-data-[open]:hidden"
+                />
+                <XMarkIcon
+                  aria-hidden="true"
+                  className="hidden size-6 group-data-[open]:block"
+                />
               </DisclosureButton>
             </div>
 
-            <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+            <div className="flex flex-1 items-center justify-center tablet:items-stretch tablet:justify-start">
               {/* Title */}
               <div className="flex shrink-0 items-center">
                 <a href="/">
-                  <strong className="text-lg bg-clip-text text-transparent bg-gradient-to-r 
-                                    from-red-500 to-orange-500"> 
-                  Omkar Portfolio </strong>
+                  <strong className="text-lg bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-orange-500">
+                    Omkar Portfolio{" "}
+                  </strong>
                 </a>
               </div>
               {/* Navigation Menu */}
-              <div className="hidden md:ml-10 md:block">
+              <div className="hidden tablet:ml-10 tablet:block">
                 <div className="flex space-x-4">
                   {navigation.map((item) => (
-                    <a key={item.name} href={item.href} 
-                      className={classNames(pathname === item.href ? 'bg-orange-600 text-white' : 
-                              'hover:underline underline-offset-4 decoration-red-600 hover:text-gray-600 dark:hover:text-gray-300',
-                              'rounded-md px-3 py-2 text-sm font-medium shrink-0')}>
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        pathname === item.href
+                          ? "bg-orange-600 text-white"
+                          : "hover:underline underline-offset-4 decoration-red-600 hover:text-gray-600 dark:hover:text-gray-300",
+                        "rounded-md px-3 py-2 text-sm font-medium shrink-0"
+                      )}
+                    >
                       {item.name}
                     </a>
                   ))}
@@ -66,18 +79,23 @@ export function Navbar() {
               </div>
             </div>
 
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto 
-                            space-x-4 md:ml-6 md:pr-0">
+            <div
+              className="absolute inset-y-0 right-0 flex items-center pr-2 tablet:static tablet:inset-auto 
+                            space-x-4 tablet:ml-6 tablet:pr-0"
+            >
               <a href="/resume">
-                <span className="md:ml-3">
-                  <button type="button" className="inline-flex items-center rounded-md 
+                <span className="tablet:ml-3">
+                  <button
+                    type="button"
+                    className="inline-flex items-center rounded-md 
                           bg-gradient-to-r from-red-600 to-orange-600 px-3 py-2 text-sm 
                           font-semibold text-white shadow-sm hover:from-red-500 
                           hover:via-orange-500 hover:to-yellow-500 focus-visible:outline 
                           focus-visible:outline-2 focus-visible:outline-offset-2 
-                          focus-visible:outline-white">
-                    View Resume
-                  </button>                
+                          focus-visible:outline-white"
+                  >
+                    Resume
+                  </button>
                 </span>
               </a>
               <ThemeSwitch />
@@ -88,10 +106,17 @@ export function Navbar() {
         <DisclosurePanel className="md:hidden">
           <div className="space-y-1 px-2 pb-3 pt-2">
             {navigation.map((item) => (
-              <DisclosureButton key={item.name} as="a" href={item.href}
-                className={classNames( pathname === item.href ? 'bg-orange-600 text-white' : 
-                'hover:underline underline-offset-4 decoration-red-600 text-gray-600 dark:text-gray-400', 
-                'block rounded-md px-3 py-2 text-base font-medium',)} >
+              <DisclosureButton
+                key={item.name}
+                as="a"
+                href={item.href}
+                className={classNames(
+                  pathname === item.href
+                    ? "bg-orange-600 text-white"
+                    : "hover:underline underline-offset-4 decoration-red-600 text-gray-600 dark:text-gray-400",
+                  "block rounded-md px-3 py-2 text-base font-medium"
+                )}
+              >
                 {item.name}
               </DisclosureButton>
             ))}
@@ -99,5 +124,5 @@ export function Navbar() {
         </DisclosurePanel>
       </div>
     </Disclosure>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
           },
         },
       },
+      screens: {
+        'tablet': "850px"
+      }
     },
   },
   plugins: [require("@tailwindcss/typography")],
