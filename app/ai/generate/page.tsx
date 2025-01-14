738cc0c8f3728b5a790dcbc1f92d60a3c3e19a4b
omkarsdalvi1 <omkarsdalvi1@gmail.com>
2025-01-14 12:55:51 +0000

    Page under transation posted

---
+++ app/ai/generate/page.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Photos: React.FC = () => {
+  return (
+    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-orange-600">Yikes!</p>
+        <h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight sm:text-7xl">
+          Page under construction.
+        </h1>
+        <p className="mt-6 text-pretty text-lg font-medium sm:text-xl/8">
+          Ai Photo Generation page is unavailabe for now. This page will be added as soon as possible.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            href="/"
+            className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm
+                    hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+                    focus-visible:outline-orange-600"
+          >
+            Go back home
+          </Link>
+          <Link href="/feedback" className="text-sm font-semibold hover:text-orange-600">
+            Provide Feedback <span aria-hidden="true">&rarr;</span>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Photos;
