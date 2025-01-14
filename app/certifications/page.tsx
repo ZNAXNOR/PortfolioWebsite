962ddc92d6926097d195c464f4d5048203bfc8fc
omkarsdalvi1 <omkarsdalvi1@gmail.com>
2025-01-14 12:52:23 +0000

    Repair Certifications.

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { fetchCertifications } from "./generate-data";
 import Image from "next/image";
 
@@ -34,7 +33,6 @@ export default async function CertificationsPage() {
   return (
     <div className="container mx-auto py-6">
       <h1 className="text-3xl font-bold mb-4">Credly Certification Badges</h1>
-      <h1 className="text-3xl font-bold mb-4">Credly Certification Badges</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {certifications.map((cert) => {
           const { id } = cert;
@@ -45,52 +43,49 @@ export default async function CertificationsPage() {
             )?.entity.name || "Unknown Issuer";
 
           return (
-            <div
-              className="max-w-sm bg-white border border-gray-200 rounded-lg shadow 
-                  dark:bg-gray-800 dark:border-gray-700 hover:drop-shadow-2xl"
-            >
-              <div className="flex px-auto text-centre place-item-centre">
-                <Link
-                  href={badge_template.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+            <a key={id} target="_blank" rel="noopener noreferrer" href={`http://www.credly.com/badges/${id}`}
+              className="hover:drop-shadow-2xl focus:ring-gray-700 focus:ring-offset-4">
+              <div className="max-w-sm h-96 flex flex-col justify-between bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                {/* Badge Image */}
+                <div className="flex justify-center p-4">
                   <Image
-                    className="rounded-t-lg"
+                    className="rounded-lg"
                     src={badge_template.image_url}
                     alt={badge_template.name}
-                    width={150}
-                    height={150}
+                    width={100}
+                    height={100}
+                    loading="lazy"
                   />
-                </Link>
-              </div>
-              <div className="p-5">
-                <a href={badge_template.url}>
-                  <h5 className="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white">
+                </div>
+
+                {/* Badge Details */}
+                <div className="p-4 flex flex-col justify-between h-full">
+                  <h5 className="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white text-center">
                     {badge_template.name}
                   </h5>
-                </a>
-                <h6
-                  className="font-bold text-gray-600 dark:text-gray-400 leading-snug tracking-normal mx-auto my-6 w-full text-sm
-                       max-w-xs lg:max-w-md"
-                >
-                  <strong>Issued by</strong> {issuer}
-                </h6>
-                <p
-                  className="m-3 p-3 text-gray-700 dark:text-gray-400 max-w-sm border bg-gray-100
-                            border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-                >
-                  {badge_template.description}
-                </p>
-                <h6
-                  className="font-bold text-gray-600 dark:text-gray-400 leading-snug tracking-normal mx-auto my-6 w-full text-sm
-                       max-w-xs lg:max-w-md"
-                >
-                  <strong>Issued on</strong>{" "}
-                  {cert.issued_at ? formatDate(cert.issued_at) : "Unknown Date"}
-                </h6>
+                  <h6 className="text-gray-600 dark:text-gray-400 text-sm text-center">
+                    <strong>Issued by:</strong> {issuer}
+                  </h6>
+
+                  {/* Description */}
+                  <p className="text-gray-700 dark:text-gray-400 text-sm mt-4 line-clamp-3">
+                    <strong>Description:</strong>{" "}
+                    {badge_template.description}
+                  </p>
+
+                  {/* Issued Date */}
+                  <h6 className="text-gray-600 dark:text-gray-400 text-sm text-center mt-4">
+                    <strong>Issued on:</strong>{" "}
+                    {cert.issued_at ? formatDate(cert.issued_at) : "Unknown Date"}
+                  </h6>
+
+                  {/* Footnote */}
+                  <p className="pt-1 font-light leading-relaxed mx-auto text-slate-500 text-sm max-w-3xl">
+                    Click the card to view more details
+                  </p>
+                </div>
               </div>
-            </div>
+            </a>
           );
         })}
       </div>
